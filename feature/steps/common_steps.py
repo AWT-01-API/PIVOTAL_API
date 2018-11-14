a5a6f78d1bf81ef58a93d277542ec4b9b3f3b208
Kevin Herrera <kevin.herrera@fundacion.jala.org>
2018-11-14 10:48:26 -0400

    fix observations of code

--- feature/steps/common_steps.py
+++ feature/steps/common_steps.py
@@ -1,12 +1,12 @@
 from behave import *
-from src.API.RequestManager import RequestManager
+from src.api.request_manager import RequestManager
 
 use_step_matcher("re")
 
 
 @given('I send a basic auth get request \"([^\"]*)\"')
 def step_impl(context, endpoint):
-    context.req_helper = RequestManager()
+    context.req_helper = RequestManager("https://www.pivotaltracker.com/services/v5", "", "")
     context.last_response = context.req_helper.get_request_basic_auth(endpoint)
 
 
