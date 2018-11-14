c3f0c4ebc2e98ac443cc0c1356aa5ee42fefc914
Angelica Lopez <elianor@gmail.com>
2018-11-14 10:42:52 -0400

    Added approach to logs.

--- src/util/logger.py
+++ src/util/logger.py
@@ -1,14 +1,21 @@
+"""
+Section imports.
+"""
+from src.util.ReadCfg import ReadCfg
 import logging
 
-class Logger:
+"""
+This class is to logs.
+"""
+class logger:
 
+    #filename read from properties.
     def __init__(self):
         # set up logging to file - see previous section for more details
         logging.basicConfig(level=logging.DEBUG,
                             format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                             datefmt='%m-%d %H:%M',
-                            filename='/temp/pivotal.log',
-                            filemode='w')
+                            filename=ReadCfg.get_value("log_path"))
         # define a Handler which writes INFO messages or higher to the sys.stderr
         console = logging.StreamHandler()
         console.setLevel(logging.INFO)
@@ -19,28 +26,37 @@ class Logger:
         # add the handler to the root logger
         logging.getLogger('').addHandler(console)
 
-        pass
-
+    """Return a message type debug.
+    @msg receive a text.
+    """
     @staticmethod
-    def debug (msg):
+    def debug(msg):
         logging.debug(msg)
 
+    """Return a message type error.
+    @msg receive a text.
+    """
     @staticmethod
     def error(msg):
         logging.error(msg)
 
+    """Return a message type info.
+    :msg receive a text.
+    """
     @staticmethod
     def info(msg):
         logging.info(msg)
 
+    """Return a message type warning.
+    :msg receive a text.
+    """
     @staticmethod
     def warning(msg):
         logging.warning(msg)
 
+    """Return a message type critical.
+    :msg receive a text.
+    """
     @staticmethod
     def critical(msg):
         logging.critical(msg)
-
-
-log = Logger()
-log.critical("critcical")
