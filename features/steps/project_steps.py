73dc56a6745eb0b2f31533d1797de8e8f5d3e695
Kevin Herrera <kevin.herrera@fundacion.jala.org>
2018-11-15 17:06:17 -0400

    post request step

--- features/steps/project_steps.py
+++ features/steps/project_steps.py
@@ -1,6 +1,6 @@
 from behave import given, step, use_step_matcher
-from src.API.request_manager import RequestManager
-from src.API.project.projects import Projects
+from src.api.request_manager import RequestManager
+from src.api.project.projects import Projects
 from src.util.ReadCfg import ReadCfg
 
 @given('I login as "user1"')
@@ -9,7 +9,6 @@ def step_impl(context):
     context.req_helper = RequestManager("https://www.pivotaltracker.com/services/v5", "", "")
     context.req_helper.set_token(key)
     context.last_response = context.req_helper.get_request("/me")
-    print (context.last_response.status_code)
 
 @given('a {name}')
 def step_impl(context, name):
--- src/API/Accounts.py
+++
@@ -1,5 +0,0 @@
-from src.util.ReadCfg import ReadCfg
-
-
-print ReadCfg.get_value("url")
-print ReadCfg.get_value("user1")
