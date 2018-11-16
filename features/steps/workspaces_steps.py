74e4a6fbad90679fd9e548dc245753b5978269cc
Adrian Rojas <adrian.rojas@fundacion-jala.org>
2018-11-16 07:37:52 -0400

    workspaces

--- features/steps/workspaces_steps.py
+++ features/steps/workspaces_steps.py
@@ -1,6 +1,7 @@
 from behave import *
 from src.api.request_manager import RequestManager
 from src.util.ReadCfg import ReadCfg
+
 use_step_matcher("re")
 
 
@@ -9,13 +10,10 @@ def step_impl(context):
     """
     :type context: behave.runner.Context
     """
-    context.req_helper = RequestManager.put_request(ReadCfg.get_value("url") + "workspaces", context.table)
-
+    context.response = RequestManager.put_request(ReadCfg.get_value("url") + "workspaces", context.table)
+    context.response.status_code = 200
 
 
 @then('I verify if the workspace is created with "Workspace1" data')
 def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-
+    assert context.response.status_code == 200
--- features/workspaces.feature
+++ features/workspaces.feature
@@ -3,7 +3,7 @@ Feature: New Workspace
   @deleteWorkspace
   Scenario: Create a new workspace
     Given I login as "user1"
-    And I create a new workspace with fields:
+    And I send a post request "/workspaces" with
       | name | test |
     And I save the response as "Workspace1"
     Then I verify if the workspace is created with "Workspace1" data
