6832cb325778d77ef8483486fbead002d2701fad
Adrian Rojas <adrian.rojas@fundacion-jala.org>
2018-11-19 09:15:07 -0400

    unittest fixed

--- test/util_test/test_readCfg.py
+++ test/util_test/test_readCfg.py
@@ -4,7 +4,7 @@ from src.util.ReadCfg import ReadCfg
 
 class TestReadCfg(TestCase):
     def test_get_value(self):
-        self.assertEqual("hapsneeze", ReadCfg.get_value("username1"))
+        self.assertEqual("hapsneeze", ReadCfg.get_value("user1"))
 
     def test_get_and_not_found(self):
         with self.assertRaises(Exception) as context:
